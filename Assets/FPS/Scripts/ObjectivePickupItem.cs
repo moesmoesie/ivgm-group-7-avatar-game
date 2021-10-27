8e5e70d562812ccbf94f65cf35d58bd5248e88dd
Kwannoo <kwansing2000@hotmail.nl>
2021-10-27 17:43:45 +0200

    extra

--- Assets/FPS/Scripts/ObjectivePickupItem.cs
+++ Assets/FPS/Scripts/ObjectivePickupItem.cs
@@ -25,7 +25,7 @@ public class ObjectivePickupItem : MonoBehaviour
 
         // this will trigger the objective completion
         // it works even if the player can't pickup the item (i.e. objective pickup healthpack while at full heath)
-        m_Objective.CompleteObjective(string.Empty, string.Empty, "Objective complete : " + m_Objective.title);
+        m_Objective.CompleteObjective(string.Empty, string.Empty, "Objective complete : Pickup Doublejump");
 
         if (gameObject)
         {
